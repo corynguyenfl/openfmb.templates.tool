b66e87bdb519433b62579955b3707a9b67e74132
Cory Nguyen <corynguyenfl@gmail.com>
2023-10-10 09:00:22 -0400

    Removed unused code

--- openfmb.templates.tool/Configurations/Plugins/Dnp3MasterPlugin.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace openfmb.templates.tool.Configurations.Plugins
-{
-    public class Dnp3MasterPlugin : BasePlugin, IPlugin
-    {
-        public override string Name => PluginsSection.Dnp3Master;
-
-        public override string SessionTagName => "masters";
-    }
-}
